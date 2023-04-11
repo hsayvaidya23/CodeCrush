4ef8a81efd7066233c211c9c633473ef906a8e05
Yash Vaidya <bobbynaldocr7@gmail.com>
2023-04-12 00:19:08 +0530

    Navbar & Footer Created

---
+++ components/Navbar.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+
+const Navbar = () => {
+  return (
+    <nav className="flex items-center justify-between mt-4 mb-10">
+      <Link href="/">
+        <div className="flex items-center cursor-pointer">
+          <Image
+            src="/logo_final.png"
+            alt="logo of website"
+            height={35}
+            width={300}
+          />
+          {/* <span className="font-bold ml-2 text-primary">
+                        CodeCrush's BLog
+                    </span> */}
+        </div>
+      </Link>
+
+      <ul className="flex items-center">
+        <li className="mr-6 font-medium text-gray-600">
+          <Link href={"/"}>Products</Link>
+        </li>
+        <li className="mr-6 font-medium text-gray-600">
+          <Link href={"/about"}>Pricing</Link>
+        </li>
+        <li className="mr-6 font-medium text-gray-600">
+          <Link href={"/about"}>Docs</Link>
+        </li>
+        <li className="mr-6 font-medium text-gray-600">
+          <Link href={"/about"}>Company</Link>
+        </li>
+      </ul>
+
+      <ul className="flex items-center">
+        <li className="mr-6 font-medium text-gray-600 text-center hover:text-black shadow-Ambidient p-3 rounded-[10px]">
+            <Link href={"#"}>Log in</Link>
+        </li>
+        <li className="font-medium text-center text-white shadow-Ambidient bg-primary p-3 rounded-[15px]  hover:bg-primary-dark transition-all">
+            <Link href={"#"}>Sign up</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,14 +8,18 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: "#53BD95",
-        'primary-dark': "#2c785c",
-      }
+        primary: "#534ff2",
+        "primary-dark": "#100bc5",
+      },
+      boxShadow: {
+        Ambidient: "0px 8px 10px 0px rgba(0, 0, 0, 0.2)",
+        Penumbra: "0px 6px 30px 5px rgba(0, 0, 0, 0.12)",
+        Umbra: "0px 16px 24px 2px rgba(0, 0, 0, 0.14)",
+      },
     },
-    fontFamily : {
+    fontFamily: {
       sans: ["Roboto", "sans-serif"],
-    }
+    },
   },
   plugins: [],
-}
-
+};
